7676a032a8da78da1a1b740516aa9161aa4d01c9
Rahul Pamnani <raghavpamnani@gmail.com>
2018-03-24 00:09:13 +0530

    Details Done

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -18,9 +18,9 @@
 	  </div>
 	 
 	 
-	  <a class="panel-block ">
+	  <a class="panel-block " v-for="item,key in lists">
 	  	<span class="column is-9">
-	    	marksheet
+	    	{{ item.name }}
 	  	</span>
 	    <span class="panel-icon column is-1">
 	      <i class="has-text-danger fa fa-trash" aria-hidden="true"></i>
@@ -28,31 +28,45 @@
 	    <span class="panel-icon column is-1">
 	      <i class="has-text-info fa fa-edit" aria-hidden="true"></i>
 	    </span><span class="panel-icon column is-1">
-	      <i class="has-text-primary fa fa-eye" aria-hidden="true"></i>
+	      <i class="has-text-primary fa fa-eye" aria-hidden="true" @click="openShow(key)"></i>
 	    </span>
 	  </a>
 	</nav>
 
 	<Add :openmodal='addActive' @closeRequest='close'></Add>
+	<Show :openmodal='showActive' @closeRequest='close'></Show>
 	</div>
 </template>
 
 
 <script>
 let Add = require('./Add.vue');
+let Show = require('./Show.vue');
 	export default{
-		components:{Add},
+		components:{Add,Show},
 		data(){
 		return{
-			addActive : ''
+			addActive : '',
+			showActive : '',
+			lists:{},
+			errors:{}
 			}
 		},
+		mounted(){
+		axios.post('/getData')
+		.then((response)=> this.lists = response.data)
+		.catch((error) => this.errors = error.response.data.errors)
+	},
 		methods:{
 			openAdd(){
 				this.addActive = 'is-active';
 			},
+			openShow(key){
+			  this.$children[1].list = this.lists[key]
+				this.showActive = 'is-active';
+			},
 			close(){
-				this.addActive = ''
+				this.addActive = this.showActive = ''
 			}
 		}
 	}
